da53249a7747b5bcea2aeb0eb45f6891a64d3513
lailailai <954806471@qq.com>
2019-04-13 19:19:35 +0800

    运营商后台增加了查询秒杀订单的的功能,并能够查看订单详情以及条件查询

--- pinyougou-web-manager/src/main/java/cn/itcast/core/controller/SeckillOrderController.java
+++ pinyougou-web-manager/src/main/java/cn/itcast/core/controller/SeckillOrderController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/seckillOrder")
-public class seckillOrderController {
+public class SeckillOrderController {
     @Reference
     private seckillOrderService seckillOrderService;
 
